5c9ac5fb8931bf0e843fb6c7b533c6e38eafef50
jamon.ben <jamon.ben@3560173e-5ab8-4e21-8e63-b8887b085805>
2008-07-08 02:38:06 +0000

    looks nicer

--- lnzscript/build.bat
+++ lnzscript/build.bat
@@ -1,5 +1,6 @@
-
+@echo off
 IF NOT EXIST Makefile GOTO notqmade 
+@echo on
 
 :: Run python scripts to generate c++
 c:\python25\python .\provide_script\source\script_createcpp.py .\provide_script\source
@@ -8,6 +9,7 @@ c:\python25\python .\provide_script\source\script_createcpp.py .\provide_script\
 make
 
 :notqmade
+@echo off
 echo No makefile. You probably need to run something like "qmake testinterfaceconsole.pro"
 echo Read "building.txt"for more information.
 echo (Quitting)
