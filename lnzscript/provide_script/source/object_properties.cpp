4fed3358bab05fbcb51a6a20747f741d73d685a7
jamon.ben <jamon.ben@3560173e-5ab8-4e21-8e63-b8887b085805>
2008-07-09 01:45:36 +0000

    Added dialog constants

--- lnzscript/provide_script/source/object_properties.cpp
+++ lnzscript/provide_script/source/object_properties.cpp
@@ -15,3 +15,8 @@ objWindow.setProperty("RESTORE", QScriptValue(eng, 9));
 objWindow.setProperty("SHOWDEFAULT", QScriptValue(eng, 10));
 objWindow.setProperty("FORCEMINIMIZE", QScriptValue(eng, 11));
 objWindow.setProperty("MAX", QScriptValue(eng, 11));
+
+objDialog.setProperty("YES", QScriptValue(eng, 2));
+objDialog.setProperty("OK", QScriptValue(eng, 2));
+objDialog.setProperty("NO", QScriptValue(eng, 1));
+objDialog.setProperty("CANCEL", QScriptValue(eng, 0));
