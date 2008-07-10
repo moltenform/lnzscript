51295bcf3e88245d416beb47d6e11160bdf429a4
jamon.ben <jamon.ben@3560173e-5ab8-4e21-8e63-b8887b085805>
2008-07-10 00:31:02 +0000

    Option to confirm

--- lnzscript/testinterfaceconsole/main.cpp
+++ lnzscript/testinterfaceconsole/main.cpp
@@ -7,14 +7,21 @@ int main(int argc, char *argv[])
 {
 	TestInterfaceConsole console;
 	
-	char doc[] = "LnzScript Console\n\tRun with /r, read from stdin until EOF\n\tRun with/f, and file name for arg 2, load .js script and run it\n\tRun with /e, and text for arg 2, execute the text and quit\n\tRun with no arguments, interactive mode.";
+	char doc[] = "LnzScript Console\n\tRun with /r, read from stdin until EOF\n\tRun with /f, and file name for arg 2, load .js script and run it\n\tRun with /fconfirm, same as /f but ask for confirmation.\n\tRun with /e, and text for arg 2, execute the text and quit\n\tRun with no arguments, interactive mode.";
 
 	if (argc == 3 && strcmp(argv[1], "/e")==0)
 	{
 		console.evaluateAndPrintResults( argv[2] );
 	}
-	else if (argc == 3 && strcmp(argv[1], "/f")==0)
+	else if (argc == 3 && (strcmp(argv[1], "/f")==0||strcmp(argv[1], "/fconfirm")==0))
 	{
+		if (strcmp(argv[1], "/fconfirm")==0)
+		{
+			// present dialog asking for confirmation
+			bool bContinue = console.getConfirmationToRunScript();
+			if (!bContinue) return 1;
+		}
+		
 		// read from file and run it
 		QString contents;
 		try
@@ -26,6 +33,12 @@ int main(int argc, char *argv[])
 		} catch (...) { std::cerr << "Error."; return 0; }
 		
 		if (contents.isEmpty()) return 0; // string was empty
+		
+		// set the current directory to the directory of the script. More intuitive for the user.
+		// (we've already recorded the previous directory before)
+		QFileInfo fileInfo(argv[2]);
+		QDir::setCurrent(fileInfo.absolutePath());
+		
 		console.evaluateAndPrintResults( contents.toLatin1() );
 	}
 	else if (argc == 2 && strcmp(argv[1], "/r")==0)
@@ -36,16 +49,7 @@ int main(int argc, char *argv[])
 		std::string strOut;
 		getline(std::cin, strOut, '\0');
 		console.evaluateAndPrintResults( strOut.c_str() );
-		
-		// Before, tried this, but "a b c" would go through 3 iterations, seemed less efficient.
-		/*
-		std::string strLine;
-		std::string strLine;
-		while (std::cin >> strLine)
-		{
-		   std::cout << "::"+strLine;
-			strOut += strLine;
-		}*/
+
 	}
 	else if (argc == 1) //run with no arguments
 	{
