799ab3469255f3872dd13a6300b1d348ec3985d7
jamon.ben <jamon.ben@3560173e-5ab8-4e21-8e63-b8887b085805>
2008-07-17 02:05:57 +0000

    version 2

--- lnzscript/util/wincommondialog/WinCommonDialog/WinCommonDialog.cpp
+++ lnzscript/util/wincommondialog/WinCommonDialog/WinCommonDialog.cpp
@@ -2,77 +2,49 @@
 // See http://msdn.microsoft.com/en-us/library/ms645505(VS.85).aspx
 
 
+// "Winmm.lib" added to External dependencies under Project settings/linker
+// "Use Unicode Character Set" turned on in Project settings
+// Pre-compiled headers turned off in Project settings
 
-#include "stdafx.h"
-//#define UNICODE
-// I would have had to convert the _TCHAR to the right format.
-
-#define WIN32_LEAN_AND_MEAN
-#include "Windows.h"
+#include "WinCommonDialog.h"
+#include "dialog_simple.h"
+#include "dialog_file.h"
+#include "dialog_color.h"
+#include "dialog_sound.h"
 
 const char * documentation = 
 "WinCommonDialog.exe, Ben Fisher 2008, GPL\n"
 "A wrapper over simple Windows dialogs, using return code to pass result.\n"
 "Part of Launchorz, http://code.google.com/p/launchorz/\n"
 "\n"
-"Syntax:\n"
-"WinCommonDialog {type} {title} {text}\n"
-"\n"
-"Types:\n"
-"yesno\treturns 2 if yes, 1 if no.\n"
-"okcancel\treturns 2 if ok, 0 if cancel.\n"
-"yesnocancel\treturns 2 if yes, 1 if no, 0 if cancel\n"
-"info\n"
-"error\n"
-"warning\n"
-;
+"This is divided into 3 different functions. For more specific help, type:\n"
+"WinCommonDialog simple /?\n"
+"WinCommonDialog color /?\n"
+"WinCommonDialog file /?\n"
+"WinCommonDialog sound /?\n"
+"\n";
 
 
-#define ErrorResult 99;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	if (argc<3) { puts(documentation); puts("\nNot enough arguments"); return ErrorResult; }
-	_TCHAR* type = argv[1];
-	_TCHAR* title = argv[2];
-	_TCHAR* text = argv[3];
-	int result;
+	if (argc<2) { puts(documentation); puts("\nNot enough arguments"); return ErrorResult; }
 
-	if (strcmp(type,"yesno")==0)
+	_TCHAR* mode = get_argument(1, argc, argv);
+	if (wcscmp(mode,_T("simple"))==0)
 	{
-		result = MessageBox(NULL, text, title,  MB_YESNO | MB_ICONQUESTION);
-		if (result==IDYES) return 2;
-		else if (result==IDNO) return 1;
-		else return ErrorResult;
+		return dialog_simple(argc - 1, &argv[1]); // Pass arguments except name of program
 	}
-	else if (strcmp(type,"okcancel")==0)
+	else if (wcscmp(mode,_T("color"))==0)
 	{
-		result = MessageBox(NULL, text, title,  MB_OKCANCEL | MB_ICONQUESTION);
-		if (result==IDOK) return 2;
-		else if (result==IDCANCEL) return 0;
-		else return ErrorResult;
+		return dialog_color(argc - 1, &argv[1]);
 	}
-	else if (strcmp(type,"yesnocancel")==0)
+	else if (wcscmp(mode,_T("file"))==0)
 	{
-		result = MessageBox(NULL, text, title,  MB_YESNOCANCEL | MB_ICONQUESTION);
-		if (result==IDYES) return 2;
-		else if (result==IDNO) return 1;
-		else if (result==IDCANCEL) return 0;
-		else return ErrorResult;
+		return dialog_file(argc - 1, &argv[1]);
 	}
-	else if (strcmp(type,"info")==0)
+	else if (wcscmp(mode,_T("sound"))==0)
 	{
-		result = MessageBox(NULL, text, title,  MB_OK | MB_ICONINFORMATION);
-		return 0;
-	}
-	else if (strcmp(type,"error")==0)
-	{
-		result = MessageBox(NULL, text, title,  MB_OK | MB_ICONERROR);
-		return 0;
-	}
-	else if (strcmp(type,"warning")==0)
-	{
-		result = MessageBox(NULL, text, title,  MB_OK | MB_ICONWARNING);
-		return 0;
+		return dialog_sound(argc - 1, &argv[1]);
 	}
 	else
 	{
@@ -83,3 +55,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+// Bounds-checking when retrieving argument. Checks if argument exists. If it doesn't quit program.
+_TCHAR* get_argument(int index, int argc, _TCHAR** argv)
+{
+	if (index >= argc)
+	{
+		puts("Not enough arguments. Run without any arguments to see doc.");
+		exit(ErrorResult);
+	}
+	return argv[index];
+}
