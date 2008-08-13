3c9bba36e5de506716f1ab41cf8da5d01ccc5136
jamon.ben <jamon.ben@3560173e-5ab8-4e21-8e63-b8887b085805>
2008-08-13 03:57:52 +0000

    Loop

--- lnzeditor/app/configuration/wscite/tools/forloop.lua
+++ lnzeditor/app/configuration/wscite/tools/forloop.lua
@@ -3,39 +3,27 @@ function GetCharAt(n)
 end
 
 function ForLoopExpand()
-	local pos = editor.CurrentPos-1
-	local firstpos = math.max(pos - 300, 0)
-
-	local sinput = ''
-	local found=false
-	-- go backwards until hitting the first newline
-	while pos > firstpos do
-		local ch = GetCharAt(pos)
-		if ch=="\n" then 
-			found = true
-			break 
-		end
-		sinput = ch .. sinput
-		pos = pos-1
-	end
-	if found==false then return false end
+	local linetext,linepos = (editor:GetCurLine())
 
 	-- it looks like you are doing this on an already complete for loop, which doesn't do any good
-	if string.find(sinput,';',1,true)~=nil or string.find(sinput,'++',1,true)~=nil then
+	if string.find(linetext,';',1,true)~=nil or string.find(linetext,'++',1,true)~=nil then
 	return false
 	end
-	
-	-- Using [(] to represent the literal (
+
+	-- [(] in regexp to represent the literal (
 	local re = 'for%s*[(]([^)]+)[)]?'
-	tstart,tend,captured = string.find(sinput,re)
+	local tstart,tend,captured = string.find(linetext,re)
 	if captured==nil then return false end
-	
+
 	local result='for(var i=0; i<'..captured..';i++)'
-	editor:remove(pos, editor.CurrentPos)
-	editor:ReplaceSel(result)
-	
+	editor:DelLineLeft() --clear line
+	editor:ReplaceSel(result) -- add output
+
 	return true
 end
+
+-- previous version used Lua string manip, and had a bug. It would delete preceding line, except on the first time run on that line.
+-- This might have had to do with CurrentPos / inconsistencies in treating \r\n as a single character
 
 local res = ForLoopExpand()
 if res~= true then
