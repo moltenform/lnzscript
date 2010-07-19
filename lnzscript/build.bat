6aac1b1e18c9df1c8cd7155c2557d4faf692ef56
jamon.ben@gmail.com <jamon.ben@gmail.com@3560173e-5ab8-4e21-8e63-b8887b085805>
2010-07-19 04:47:28 +0000

    Includes should be relative to the current file, not project dir.

--- lnzscript/build.bat
+++ lnzscript/build.bat
@@ -3,16 +3,19 @@ IF NOT EXIST Makefile GOTO notqmade
 @echo on
 
 :: Run python scripts to generate c++
-c:\python25\python .\provide_script\source\script_createcpp.py .\provide_script\source
+C:\python27\python .\provide_script\source\script_createcpp.py .\provide_script\source
 
 :: Run python scripts to generate JavaScript lib
-c:\python25\python .\provide_script\source\script_create_std_js.py .\provide_script\source .\release
+C:\python27\python .\provide_script\source\script_create_std_js.py .\provide_script\source .\release
 
 :: run make
 make
+GOTO end
 
 :notqmade
 @echo off
 echo No makefile. You probably need to run something like "qmake lnzconsole.pro"
 echo Read "building.txt"for more information.
 echo (Quitting)
+
+:end
