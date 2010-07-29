4304fda3373d6342dc0a04400b56e2799086a4a0
jamon.ben@gmail.com <jamon.ben@gmail.com@3560173e-5ab8-4e21-8e63-b8887b085805>
2010-07-29 04:24:12 +0000

    More build additions

--- lnzscript/build.bat
+++ lnzscript/build.bat
@@ -2,11 +2,13 @@
 IF NOT EXIST Makefile GOTO notqmade 
 @echo on
 
+set PYTHONDIR=c:\python27
+
 :: Run python scripts to generate c++
-C:\python27\python .\provide_script\source\script_createcpp.py .\provide_script\source
+call %PYTHONDIR%\python .\provide_script\source\script_createcpp.py .\provide_script\source
 
 :: Run python scripts to generate JavaScript lib
-C:\python27\python .\provide_script\source\script_create_std_js.py .\provide_script\source .\release
+call %PYTHONDIR%\python .\provide_script\source\script_create_std_js.py .\provide_script\source .\release
 
 :: run make
 make
