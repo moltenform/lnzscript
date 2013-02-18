70b95dac9ec980de8098b6a0730f77ed6baf36d9
Ben Fisher <jamon.ben@gmail.com>
2013-02-18 00:30:40 -0800

    fix minor parsing bug

--- lnzscript/provide_script/parse_js.cpp
+++ lnzscript/provide_script/parse_js.cpp
@@ -46,14 +46,16 @@ QString processLiteralStrings(const QString& strInput)
 				strParsed[len++] = BSLASH;
 				strParsed[len++] = BSLASH;
 			}
-			else if (strInput[i] == BSLASH && strInput[i+1] == SQUOTE && state == SINGLE_QUOTE) // 'this is \' one long string' , backslash escapes closing '
+			// 'this is \' one long string' , backslash escapes closing '
+			else if (strInput[i] == BSLASH && strInput[i+1] == SQUOTE && (i-1>=0) && strInput[i-1] != BSLASH && state == SINGLE_QUOTE) 
 			{
 				//remain in the SINGLE_QUOTE state
 				strParsed[len++] = strInput[i];
 				i++; //skip to next char
 				strParsed[len++] = strInput[i];
 			}
-			else if (strInput[i] == BSLASH && strInput[i+1] == DQUOTE && state == DOUBLE_QUOTE) // "this is \" one long string" , backslash escapes closing "
+			// "this is \" one long string" , backslash escapes closing "
+			else if (strInput[i] == BSLASH && strInput[i+1] == DQUOTE && (i-1>=0) && strInput[i-1] != BSLASH && state == DOUBLE_QUOTE)
 			{
 				//remain in the DOUBLE_QUOTE state
 				strParsed[len++] = strInput[i];
@@ -101,7 +103,7 @@ QString processLiteralStrings(const QString& strInput)
 
 
 /*
-This is the old version, which did not take into account comments.
+We need to take into account comments.
 You might think, why is that important? What would be wrong about changing quotes inside a comment?
 The following fails:
 
@@ -110,59 +112,4 @@ now /here/ is some \other\ code //gets messed up
 
 It thinks that a string was starting.
 
-QString parseLiteralStrings(const QString& strInput)
-{
-	if (! strInput.contains(QString("@")))
-		return strInput; //no @ in the string, so don't do anything
-	
-	QString strParsed;
-	strParsed.reserve( strInput.length() + 5); //allocate as much as the input + 5. Just an estimate, doesn't matter if we are over/under
-	int len = 0;
-	
-	enum states {NO_QUOTE, SINGLE_QUOTE, DOUBLE_QUOTE, SINGLE_TRANSFORM};
-	#define SQUOTE '\''
-	#define DQUOTE '"'
-	#define BSLASH '\\'
-	int state = NO_QUOTE;
-	int i;
-	for (i=0;i<strInput.length()-1; i++ )
-	{
-		if (strInput[i]=='@' && strInput[i+1]==SQUOTE && state==NO_QUOTE)
-		{
-			state = SINGLE_TRANSFORM;
-			strParsed[len++] = SQUOTE;
-			i ++; // skip over the next char!
-		}
-		else if (strInput[i]==SQUOTE)
-		{
-			if (state==NO_QUOTE) state = SINGLE_QUOTE;
-			else if (state==SINGLE_QUOTE) state = NO_QUOTE;
-			else if (state==DOUBLE_QUOTE) state = DOUBLE_QUOTE; // no op
-			else if (state==SINGLE_TRANSFORM) state = NO_QUOTE; // end the transform
-			
-			strParsed[len++] = SQUOTE; //regardless, write it over
-		}
-		else if (strInput[i]==DQUOTE)
-		{
-			if (state==NO_QUOTE) state = DOUBLE_QUOTE;
-			else if (state==SINGLE_QUOTE) state = SINGLE_QUOTE; // no op
-			else if (state==DOUBLE_QUOTE) state = NO_QUOTE; // end quote
-			else if (state==SINGLE_TRANSFORM) state = SINGLE_TRANSFORM; // no op
-			
-			strParsed[len++] = DQUOTE; //regardless, write it over
-		}
-		else if (strInput[i] == BSLASH && state == SINGLE_TRANSFORM)
-		{
-			// add two backslashes!
-			strParsed[len++] = BSLASH;
-			strParsed[len++] = BSLASH;
-		}
-		else
-		{
-			strParsed[len++] = strInput[i];
-		}
-	}
-	strParsed[len++] = strInput[i]; //we don't transform the last one, anyways.
-	return strParsed;
-}
 */
